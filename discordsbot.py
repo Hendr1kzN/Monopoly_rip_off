28be613146501d98ae3b6d35f45784f93fbbb2e0
Hendrik <hendrik@goebs.eu>
2023-01-11 00:43:34 +0100

    the bot can now make a lobby and people can join and leave, I don't know if it stops at 6 players

--- discordsbot.py
+++ discordsbot.py
@@ -15,6 +15,7 @@ CHANNEL_NAME = 'testen-hier'
 
 
 intents=discord.Intents.all()
+intents.message_content = True
 discord_bot = commands.Bot(command_prefix='!', intents=intents)
 
 
@@ -42,15 +43,49 @@ async def leave(ctx):
 
 
 @discord_bot.command(name="do_poll")
-async def ex(ctx):
-    channel = ctx.message.channel
-
+async def do_poll(ctx):
     vote = discord.Embed(title="**[Monopoly]**", description="It's time to join the lobby", color=0x00ff00)
     message = await ctx.send(embed=vote)
-    reaction = await message.add_reaction("✅")
+    await message.add_reaction("✅")
+    await message.add_reaction("❌")
+    
+    def check(reaction, user):
+        return str(reaction.emoji) in ['✅', '❌']
+       
+    counter = 0
+    while True:
+        try:
 
-    async for user in reaction.users():
-        await channel.send(f'{user} has reacted with {message.emoji}!')
+            reaction, user = await discord_bot.wait_for("reaction_add", timeout=60, check=check)
+            user_id = user.mention
+           
+
+            if str(reaction.emoji) == "✅":
+                if len(list_of_players)>=6:
+                    await ctx.send(f"Sorry, {user_id}. The lobby is full.")
+                    await message.remove_reaction(reaction, user)
+                elif not user_id in list_of_players:
+                    list_of_players.append(user_id)
+                    await ctx.send(f'{user_id} joined lobby')
+                    await message.remove_reaction(reaction, user)
+                else:
+                    await ctx.send(f"{user_id} you're already in the lobby")
+                    await message.remove_reaction(reaction, user)
+                    
+            elif str(reaction.emoji) == "❌":
+                if user_id in list_of_players:
+                    list_of_players.remove(user_id)
+                    await ctx.send(f"{user_id} left the lobby")
+                    await message.remove_reaction(reaction, user)
+                else: 
+                    await ctx.send(f"{user_id} you're not in the lobby")
+                    await message.remove_reaction(reaction, user)
+            else:
+                await message.remove_reaction(reaction, user)
+                
+        except asyncio.TimeoutError:
+            await message.delete()
+            break
 
 
 @discord_bot.command(name="join_lobby")

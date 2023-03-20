2806383296100560b67e903182d10b5145375349
Hendrik <hendrik@goebs.eu>
2023-03-20 20:11:36 +0100

    added shit in squares

--- squares.py
+++ squares.py
@@ -1,8 +1,10 @@
 class Square:
-    def __init__(self, name, square_type, price, cordx, cordy):
+    def __init__(self, name, square_type, price, rent, cordx, cordy):
         self.name = name
         self.type = square_type
         self.price = price
+        self.rent = rent
+        self.house_level = 0
         self.owner = None
         self.cordx = cordx
         self.cordy = cordy
@@ -10,13 +12,14 @@ class Square:
     def land_on(self, player):
         if self.type == "property":
             if self.owner is None:
-                # Offer to buy property
-                pass
+                if input()=="buy":
+                    self.owner = player
+                    player.pay_money(self.price)
             elif self.owner == player:
                 # You already own this property
                 pass
             else:
-                # Pay rent
+                player.pay_money(self.price)
                 pass
         elif self.type == "payment":
             # player needs to pay price
@@ -28,8 +31,7 @@ class Square:
             # Draw a community chest card
             pass
         elif self.type == "go to jail":
-            # Go to jail
-            pass
+            player.go_to_jail()
     
     def __str__(self):
         return "name: "+ self.name + ", type: " + str(self.type) + ", price: " + str(self.price) + ", owner: " + str(self.owner)
